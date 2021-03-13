28c400c5fccbcf85b4facb10c9f5dca3f92bdda3
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-03-13 18:49:25 -0300

    🚧 Include comment at footer file

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -2,6 +2,12 @@
     Copyright <?php echo date('Y'); ?> | Todos os direitos reservados
 </footer>
 
+<?php 
+/*
+* We include our scripts.php file using this snippet. The %scripts_template% string will
+* be replaced by our file content when Renderer it's loading our website.
+*/
+?>
 %scripts_template%
 
 <script type="text/javascript" src="assets/app.js"></script>
