0ba1e3695e394b3f0d25e5c9eb7ef7898e0c5726
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-03-13 17:48:29 -0300

    👄 Include new localization strings

--- components/Language.php
+++ components/Language.php
@@ -1,6 +1,44 @@
 <?php
     return [
-        'section_1' => [
-            'key_1' => 'value 1',
-        ]
+        'section_headline' => [
+            'headline_principal' => 'Torne-se um <span class="text-champagne">ADVOGADO EXPERT</span> em prospecção de clientes',
+            'headline_secundaria' => 'Aprenda como sair do zero aos primeiros clientes na advocacia em menos de 30 dias',
+            'headline_form_title' => 'INSCREVA-SE',
+            'headline_youtube_1' => 'Aulas ao vivo no YouTube',
+            'headline_youtube_2' => 'Todas as segundas, às 20h',
+        ],
+
+        'section_about_course' => [ 
+            'item_1' => [
+                'title' => 'O que vou aprender nessas aulas?',
+                'content' => 'Você vai aprender o que a Faculdade de Direito e a OAB não te ensinam: <strong class="text-champagne">EMPREENDER na Advocacia Autônoma</strong> captando seus primeiros clientes em menos de 30 dias.',
+            ],
+            'item_2' => [
+                'title' => 'O que é um <strong class="text-champagne">ADVOGADO EXPERT?</strong>',
+                'content' => 'É o Advogado que destaca-se como autônomo e domina com maestria a prospecção dos seus clientes, utilizando estratégias eficientes para fechar negócio.',
+            ],
+         ],
+        
+        'section_about_bruna' => [ 
+            'title' => '', 
+            'paragraph' => [
+                'Meu nome é Bruna Alam, sou advogada com 8 anos de experiência prática no dia a dia da Advocacia Autônoma e como Correspondente.',
+                'Pós-graduanda em Direito Extrajudicial e fundadora do Escritório Alam Advocacia Extrajudicial.',
+                '<strong>Sou uma inconformada com a Advocacia tradicional</strong> e com as Faculdades de Direito que não nos ensinam a advogar! 
+                    Depois de 6 anos tentando fugir do Direito, finalmente encontrei meu lugar ao sol dentro da Advocacia Empreendedora.',
+                'Já fui Juíza Leiga, Conciliadora Criminal, Analista financeira, Gerente de Projetos e concurseira.',            
+            ],
+        ],
+        
+        'components_callouts' => [ 
+            'if_you_read_title' => 'Se você leu até aqui é porque o conteúdo te interessa', 
+            'if_you_read_subscribe' => 'Faça sua inscrição. É fácil, simples e rápido!', 
+        ],
+        
+        'components_elements' => [ 
+            'form_button' => 'Quero ser um advogado expert',
+            'copyright_main_text' => 'Copyright Jovem Advogando | Todos os direitos reservados',
+            'copyright_website' => 'https://www.jovemadvogando.com.br',
+            'copyright_email' => 'contato@jovemadvogando.com.br',
+        ],
     ];
