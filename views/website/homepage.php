54562f2003067ecfb1a77154ca668e2d8d74ad5f
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-03-13 18:48:42 -0300

    🚧 wip - Include basic homepage struct

--- views/website/homepage.php
+++ views/website/homepage.php
@@ -1,1 +1,62 @@
+<!-- SECTION :: Headline -->
+<section id="section__headline">
+    <!-- Left side, our expert profile image -->
 
+    <!-- Right side, our form and headline -->
+        <!-- YouTube row -->
+        <!-- Headline row -->
+        <!-- Subtitle row -->
+        <!-- Form container -->
+            <!-- Form title -->
+            <!-- Input group -->
+                <!-- Label -->
+                <!-- Input -->
+            <!-- Input group -->
+                <!-- Label -->
+                <!-- Input -->
+            <!-- Input group -->
+                <!-- Label -->
+                <!-- Input -->
+            <!-- Input group -->
+                <!-- Button -->
+</section>
+
+<!-- SECTION :: About class -->
+<section id="section__about_class">
+    <!-- First row -->
+        <!-- Left column -->
+            <!-- Image Container -->
+                <!-- Image -->
+        <!-- Right column -->
+            <!-- Paragraph container -->
+    
+    <!-- Second row -->
+        <!-- Left column -->
+            <!-- Image Container -->
+                <!-- Image -->
+        <!-- Right column -->
+            <!-- Paragraph container -->
+
+</section>
+
+<!-- SECTION :: About instructor -->
+<section id="section__about_instructor">
+    <!-- About card container -->
+        <!-- Left column -->
+            <!-- Image profile -->
+
+        <!-- Right column -->
+            <!-- About title -->
+            <!-- About paragraphs -->
+                <!-- Paragraph element -->
+</section>
+
+<!-- SECTION :: Callout -->
+<section id="section__callout">
+    <!-- Title container -->
+        <!-- Paragraph element -->
+    <!-- Subtitle container -->
+        <!-- Paragraph element -->
+    <!-- Button container -->
+        <!-- Button element -->
+</section>
