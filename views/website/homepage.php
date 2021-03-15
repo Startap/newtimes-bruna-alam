775418017a0357e2959ef33a359b0eff221a38d0
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-03-15 12:15:28 -0300

    🚧 Add header to our website

--- views/website/homepage.php
+++ views/website/homepage.php
@@ -1,24 +1,44 @@
 <!-- SECTION :: Headline -->
 <section id="section__headline">
     <!-- Left side, our expert profile image -->
+    <div id="section__headline_left_side"></div>
 
     <!-- Right side, our form and headline -->
+    <div id="section__headline_right_side">
         <!-- YouTube row -->
-        <!-- Headline row -->
-        <!-- Subtitle row -->
-        <!-- Form container -->
-            <!-- Form title -->
-            <!-- Input group -->
-                <!-- Label -->
-                <!-- Input -->
-            <!-- Input group -->
-                <!-- Label -->
-                <!-- Input -->
-            <!-- Input group -->
-                <!-- Label -->
-                <!-- Input -->
-            <!-- Input group -->
-                <!-- Button -->
+        <div id="headline__right_youtube_container">
+            <img src="/assets/images/icons/youtube.svg" />
+            <div>
+                <span><?php echo $this->language['section_headline']['headline_youtube_1'] ?></span>
+                <span><?php echo $this->language['section_headline']['headline_youtube_2'] ?></span>
+            </div>
+        </div>
+
+        <div id="headline__right_main_container">
+            <!-- Headline row -->        
+            <h1><?php echo $this->language['section_headline']['headline_principal'] ?></h1>
+            <!-- Subtitle row -->
+            <h2><?php echo $this->language['section_headline']['headline_secundaria'] ?></h1>
+            <!-- Form container -->
+            <div id="headline__right_form_container">
+                <h3><?php echo $this->language['section_headline']['headline_form_title'] ?></h3>
+    
+                <form action="#" method="post">
+                    <label for="complete_name">Nome completo</label>
+                    <input type="text" name="complete_name" id="complete_name">
+
+                    <label for="complete_name">E-mail para contato</label>
+                    <input type="text" name="email" id="form_email">
+
+                    <label for="complete_name">Telefone (com DDD)</label>
+                    <input type="text" name="phone" id="form_phone">
+
+                    <button class="call-to-action" type="submit">Quero ser um advogado expert</button>
+                </form>
+            </div>
+
+        </div>
+    </div>
 </section>
 
 <!-- SECTION :: About class -->
