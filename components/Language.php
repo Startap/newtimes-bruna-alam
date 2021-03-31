9a1459faec8a6902eef25761e6ba3577ff890b60
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-03-31 14:53:23 -0300

    📃 Update Language file to start sales page

--- components/Language.php
+++ components/Language.php
@@ -27,6 +27,12 @@
                     Depois de 6 anos tentando fugir do Direito, finalmente encontrei meu lugar ao sol dentro da Advocacia Empreendedora.',
                 'Já fui Juíza Leiga, Conciliadora Criminal, Analista financeira, Gerente de Projetos e concurseira.',            
             ],
+            'paragraph_sales_page' => [
+                'Uma advogada empreendedora inconformada com a Advocacia tradicional, com mais de 8 anos de experiência prática no mundo jurídico.',
+                'Quando decidi abrir meu escritório de Advocacia consegui, em 30 dias, fechar 6 contratos usando apenas meus métodos (e que vou ensinar nesta mentoria).',
+                'De Advogada Correspondente a Juíza Leiga, minha experiência no campo de batalha fala por si só.',
+                'Hoje minha missão é ensinar tudo aquilo que a Faculdade de Direito não ensina a quem quer advogar!',
+            ]
         ],
         
         'components_callouts' => [ 
@@ -56,4 +62,99 @@
             'whatsapp' => 'https://chat.whatsapp.com/EcfEXcrxA5bGUidmCeUw0W',
             'telegram' => 'https://t.me/jovemadvogando',
         ],
+
+        'sales_page' => [
+            'callout_message' => 'Aprenda como sair do Zero aos primeiros clientes na Advocacia em menos de 30 dias e faturar até R$7.000,00',
+            'targets' => [
+                'Estudantes de Direito',
+                'Jovens advogados',
+                'Bacharéis em Direito',
+            ],
+            'modules' => [
+                [ 'title' => 'Módulo 1', 'date' => '17/abril', 'description' => 'O que a Faculdade de Direito não te ensina: meios de atração de clientes' ],
+                [ 'title' => 'Módulo 2', 'date' => '18/abril', 'description' => 'Prospecção e reunião com clientes e a precificação de serviços jurídicos' ],
+                [ 'title' => 'Módulo 3', 'date' => '24/abril', 'description' => '3 formas práticas de começar a ganhar dinheiro e experiência na Advocacia - O passo a passo' ],
+                [ 'title' => 'Módulo Bônus', 'date' => '25/abril', 'description' => 'Marketing jurídico na prática - como atrair clientes através das redes sociais' ],
+            ],
+            'bonus' => [
+                'title' => 'Conteúdo do módulo bônus',
+                'bonus_content' => [
+                    [
+                        'title' => 'Marketing jurídico', 
+                        'price' => 'R$ 297,00', 
+                        'description' => [
+                            'Introdução ao Marketing jurídico', 
+                            'Nicho e público alvo', 
+                            'Cuidados e vedações do Código de Ética',
+                            'Linhas editoriais'
+                        ]
+                    ],
+                    [
+                        'title' => 'Guia do Jovem Advogado, o e-book completo', 
+                        'price' => 'R$ 97,00', 
+                        'description' => [
+                            'Reunião com clientes', 
+                            'Precificação de honorários', 
+                            'Advocacias por Correspondência, Dativa, Conciliação e Juízes Leigos',
+                            'Advogando na prática',
+                            'Audiências'
+                        ]
+                    ],
+                    [
+                        'title' => 'Modelos de postagens profissionais para Instagram', 
+                        'price' => 'R$ 47,90', 
+                        'description' => [
+                            '50 modelos práticos para copiar e colar', 
+                            'Crie conteúdos praticamente infinitos para seu perfil profissional', 
+                        ]
+                    ],
+                ]
+            ],
+            'investiment' => [
+                'title' => 'Qual o investimento desta mentoria?',
+                'subtitle' => 'Um investimento com valor promocional de R$ 497,00 nas primeiras 24 horas!',
+                'price_original' => 'R$ 3.390,00',
+                'price_discount' => 'R$ 997,00 (ou 12x de 97,14)',
+            ],
+            'faq_questions' => [
+                [
+                    'question'=> 'Afinal, o que é o curso e para que serve?',
+                    'answer' => 'É uma mentoria. Nossa meta é ajudar você a se desenvolver e conseguir prospectar mais clientes, aumetando seu faturamento como jovem advogado autônomo.'
+                ],
+                [
+                    'question'=> 'Como eu posso me inscrever?',
+                    'answer' => 'É muito fácil fazer parte desta turma. Basta clicar em qualquer um dos botões desta página escritos <b>QUERO ME INSCREVER</b>. As inscrições estão abertas por tempo limitado e de forma promocional. <b>Aproveite!</b>'
+                ],
+                [
+                    'question'=> 'Quando terei meu acesso liberado?',
+                    'answer' => 'Usamos uma plataforma que processa seu pagamento de forma automatizada. Assim que ele for processado você receberá, em seu e-mail, usuário e senha de acesso.'
+                ],
+                [
+                    'question'=> 'Como eu acesso as aulas?',
+                    'answer' => 'As aulas estão disponíveis de forma online. Assim você pode acessar de qualquer lugar, em qualquer horário, de qualquer dispositivo.'
+                ],
+                [
+                    'question'=> 'Por quanto tempo as aulas ficarão disponíveis?',
+                    'answer' => 'As aulas ficam disponíveis por <b>um ano</b> após a liberação de usuário e senha. Acreditamos que esse seja o tempo necessário para colocar todos os ensinamentos em prática.'
+                ],
+                [
+                    'question'=> 'Há alguém para tirar dúvidas durante o curso?',
+                    'answer' => 'Sim. Temos grupos no Telegram e no WhatsApp criados especialmente para isso: troca e ajuda durante a mentoria. Além disso também temos a área de membros que possui espaço para conversa e dúvidas.'
+                ],
+                [
+                    'question'=> 'O que é a <b>garantia incondicional?</b>',
+                    'answer' => 'A garantia incondicional é um direito seu. Após o pagamento você tem SETE dias para decidir se vai continuar com os estudos ou não. Se não gostou do conteúdo pode pedir seu dinheiro de volta através da plataforma de pagamento. <b>Sem pegadinhas.</b>'
+                ],
+                [
+                    'question'=> 'Estou pensando em entrar na próxima turma. Posso?',
+                    'answer' => 'A gente, sinceramente, não recomenda que você faça isso. Não temos previsão de quando haverá outra turma e as vagas desta são limitadas. Aproveite que você já está aqui e efetive sua inscrição.'
+                ],
+            ],
+
+            'disclaimer' => [
+                'title' => 'Informações sobre obtenção de resultados',
+                'text' => 'Este produto não garante a obtenção de resultados.<br><br>Qualquer referência ao desempenho de uma estratégia não deve ser interpretada como garantia de resultados.',
+            ],
+        ],
+
     ];
