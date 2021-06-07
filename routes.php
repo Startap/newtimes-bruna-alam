2157da3f8de419746ab3e3cd569a3c6d2d12073f
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-06-07 15:26:05 +0000

    🔀 Include new route to webpage

--- routes.php
+++ routes.php
@@ -6,4 +6,7 @@ use Components\Application;
 /** Call methods inside PageController when hits some path mapped on argument 1 */
 Router::route('/', '\Components\Controllers\PageController@getHomepage');
 Router::route('/obrigado', '\Components\Controllers\PageController@getThankYou');
+
 Router::route('/mentoria', '\Components\Controllers\PageController@getSalePage');
+
+Router::route('/advogado-ceo', '\Components\Controllers\PageController@getSalePage');
